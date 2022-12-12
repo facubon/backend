363346369e9b9974b5087ea5c119a40c54d25354
Facundo Pietrobon <facundopietrobon@hotmail.com>
2022-12-12 00:25:44 -0300

    actualizo

--- filesystem.js
+++ filesystem.js
@@ -26,32 +26,63 @@ class ProductManager {
             stock,
         }
 
-        //----incremento el id a medida que se agregan----/
-        if(this.Products.length === 0) {
-            producto ["id"] = 1;
-            await fs.promises.writeFile(this.filename, JSON.stringify(this.products, null, '\t'));
-        } else {
-            //Llamo al ultimo elemento del array y le sumo uno//
-            producto ["id"] = this.Products [this.Products.length -1] ["id"] + 1;
-        }
-    
+       
 
         //----cargo los productos al array---/
         this.products.push(producto);
 
         //----creo la hoja productos.json con el writefile y con stringify convierto el objeto a una cadena de texto---//
-        await fs.promises.writeFile('./productos.json', JSON.stringify(this.products, null, '\t'))
+       await fs.promises.writeFile('./productos.json', JSON.stringify(this.products, null, '\t'))
     }
 
     //----con readfile leo la hoja creada de productos.json -----/
     async getProducts (){
         return JSON.parse(await fs.promises.readFile('./productos.json'))
     }
+
+
+    //-----Busco el ID del producto-----//
+    getProductsById (id){
+        try{
+            return this.products.filter(producto => producto.id ===id);
+        }catch (error){
+            return "Error no existe este producto"
+        }
+        
+    }
+
+
 }
 
-//----borro el producto pasandole el numero de id correspondiente---//
+    //----incremento el id a medida que se agregan----/
+    save(producto){
+        if(this.Products.length === 0) {
+          producto ["id"] = 1;
+      
+     } else {
 
-//async deleteProduct (id)
+     //Llamo al ultimo elemento del array y le sumo uno//
+     producto ["id"] = this.Products [this.Products.length -1] ["id"] + 1;
+
+     this.products.push(producto);
+
+    
+        
+    
+ }}
+
+
+ deleteProduct (){
+    fs.unlink(this.filename)
+ }
+
+    
+
+    
+
+    
+        
+    
 
 
 //------cargo los productos en la hoja productos.json y les paso su contenido al indice que nombre mas arriba en createProduct-------//
@@ -65,6 +96,12 @@ updateProduct.createProduct({
     code:'abc123',
     stock:25,
 
+    title: 'asdasdasdasd',
+    description:'este es un producto prueba',
+    price: 200,
+    thumbnail:'sin imagen',
+    code:'abc123',
+    stock:20,
     
 
 })
